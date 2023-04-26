a75c0e550e18a7bb66200942eee2d883c219c274
Adam Stefański <adamtonki@gmail.com>
2023-04-26 18:28:06 +0200

    Add project

---
+++ Model/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module pl.kompo.model {
+    requires org.apache.commons.lang3;
+    requires org.slf4j;
+    requires java.sql;
+
+    opens pl.kompo.model;
+    exports pl.kompo.model;
+    exports pl.kompo.model.exceptions;
+    exports pl.kompo.model.dao;
+    opens pl.kompo.model.dao;
+}
